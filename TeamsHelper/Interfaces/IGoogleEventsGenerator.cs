a557a3c952144ee476682047e69cd5d801588471
Kacper Faber <kacperf1234@gmail.com>
2020-10-20 13:47:02 +0200

    Created GoogleEventsGenerator.

---
+++ TeamsHelper/Interfaces/IGoogleEventsGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TeamsHelper.CalendarApi;
+using TeamsHelper.TeamsApi;
+
+namespace TeamsHelper
+{
+    public interface IGoogleEventsGenerator
+    {
+        Task<List<Event>> GenerateAsync(IEnumerable<TeamsEvent> events);
+    }
+}
