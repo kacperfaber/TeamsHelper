bd67697cbef7f01879f74236432f27a09ed38e61
Kacper Faber <kacperf1234@gmail.com>
2020-10-20 10:55:23 +0200

    Teams models.

---
+++ TeamsHelper/Models/TeamsEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TeamsHelper
+{
+    public class TeamsEvent
+    {
+        [JsonProperty("@odata.etag")]
+        public string ETag { get; set; }
+        
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        
+        [JsonProperty("subject")]
+        public string Subject { get; set; }
+        
+        [JsonProperty("body")]
+        public TeamsEventBody Body { get; set; }
+        
+        [JsonProperty("start")]
+        public TeamsTime Start { get; set; }
+        
+        [JsonProperty("end")]
+        public TeamsTime End { get; set; }
+        
+        [JsonProperty("organizer")]
+        public TeamsEventOrganizer TeamsOrganizer { get; set; }
+    }
+}
---
+++ TeamsHelper/Models/TeamsEventBody.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace TeamsHelper
+{
+    public class TeamsEventBody
+    {
+        [JsonProperty("contentType")]
+        public string ContentType { get; set; }
+        
+        [JsonProperty("content")]
+        public string Content { get; set; }
+    }
+}
