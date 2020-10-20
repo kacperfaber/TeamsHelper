aa1fa0bbe51f33f2a47aae33033adeb1c6065e68
Kacper Faber <kacperf1234@gmail.com>
2020-10-20 10:55:23 +0200

    Teams models.

---
+++ TeamsHelper/Models/TeamsEmailAddress.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace TeamsHelper
+{
+    public class TeamsEmailAddress
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        [JsonProperty("address")]
+        public string Address { get; set; }
+    }
+}
---
+++ TeamsHelper/Models/TeamsTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TeamsHelper
+{
+    public class TeamsTime
+    {
+        [JsonProperty("dateTime")]
+        public DateTime DateTime { get; set; }
+        
+        [JsonProperty("timeZone")]
+        public string TimeZone { get; set; }
+    }
+}
