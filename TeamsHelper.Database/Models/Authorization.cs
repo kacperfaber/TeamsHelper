1733681a281887a11b2746c5b84ecfb16687f4b0
Kacper Faber <kacperf1234@gmail.com>
2020-10-20 11:37:35 +0200

    Added Id to Authorization, and foreigns to User.

--- TeamsHelper.Database/Models/Authorization.cs
+++ TeamsHelper.Database/Models/Authorization.cs
@@ -1,7 +1,13 @@
-﻿namespace TeamsHelper.Database
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeamsHelper.Database
 {
     public class Authorization
     {
+        [Key]
+        public Guid Id { get; set; }
+        
         public string AccessToken { get; set; }
 
         public string RenewToken { get; set; }
