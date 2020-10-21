950bf8a961d25992c636ee7bc3509d99c3680514
Kacper Faber <kacperf1234@gmail.com>
2020-10-21 09:34:40 +0200

    Created LoginController.

---
+++ TeamsHelper.WebApp/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamsHelper.WebApp
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View("Login", new LoginViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult LoginPost(LoginViewModel loginViewModel)
+        {
+            return Content($"login: {loginViewModel.Email}\npass: {loginViewModel.Password}");
+        }
+    }
+}
---
+++ TeamsHelper.WebApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeamsHelper.WebApp
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        [MinLength(5), MaxLength(15)]
+        public string Password { get; set; }
+    }
+}
