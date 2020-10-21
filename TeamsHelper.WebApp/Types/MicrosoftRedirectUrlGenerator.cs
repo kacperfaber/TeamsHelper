8c002af550eb4b1d1726a49f8b82b120eec00991
Kacper Faber <kacperf1234@gmail.com>
2020-10-21 15:26:19 +0200

    Configured for microsoft.

--- TeamsHelper.WebApp/Types/MicrosoftRedirectUrlGenerator.cs
+++ TeamsHelper.WebApp/Types/MicrosoftRedirectUrlGenerator.cs
@@ -6,7 +6,7 @@ namespace TeamsHelper.WebApp
     {
         public Task<string> GenerateAsync(OAuthConfiguration authConfiguration, string responseType)
         {
-            return Task.Run(() => $"https://login.microsoftonline.com/466d5c5a-54d4-45fc-98eb-7877acf57f5f/oauth2/v2.0/authorize?client_id={authConfiguration.ClientId}&scope=offline_access%20{authConfiguration.Scopes}&response_type={responseType}");
+            return Task.Run(() => $"https://login.microsoftonline.com/466d5c5a-54d4-45fc-98eb-7877acf57f5f/oauth2/v2.0/authorize?client_id={authConfiguration.ClientId}&scope=offline_access%20{authConfiguration.Scopes}&response_type={responseType}&redirect_uri={authConfiguration.RedirectUrl}");
         }
     }
 }
