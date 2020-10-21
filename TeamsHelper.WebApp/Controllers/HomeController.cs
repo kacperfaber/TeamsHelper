977e0d63c4acdb8e4e12c2ce093e92306d92722f
Kacper Faber <kacperf1234@gmail.com>
2020-10-21 10:16:21 +0200

    Created SetupController, HomeController, UserStore.

---
+++ TeamsHelper.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamsHelper.WebApp
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return Content("home.");
+        }
+    }
+}
