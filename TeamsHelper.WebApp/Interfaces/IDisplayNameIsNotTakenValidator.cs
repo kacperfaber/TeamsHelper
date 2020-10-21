ca827b44ea025f3a1bb42d9a1f5482651d545137
Kacper Faber <kacperf1234@gmail.com>
2020-10-21 09:51:52 +0200

    Created DisplayName and Email - IsTakenValidator.

---
+++ TeamsHelper.WebApp/Interfaces/IDisplayNameIsNotTakenValidator.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace TeamsHelper.WebApp
+{
+    public interface IDisplayNameIsNotTakenValidator
+    {
+        Task<bool> ValidateAsync(string displayName);
+    }
+}
---
+++ TeamsHelper.WebApp/Interfaces/IUserGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using TeamsHelper.Database;
+
+namespace TeamsHelper.WebApp
+{
+    public interface IUserGenerator
+    {
+        Task<User> GenerateAsync(RegisterViewModel registerViewModel);
+    }
+}
