50575b8e2cdb813570cefb78165570d86d24ae30
Kacper Faber <kacperf1234@gmail.com>
2020-10-23 15:20:45 +0200

    Added GoogleEventValidator.

---
+++ TeamsHelper.WebApp/Interfaces/IGoogleEventValidator.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using TeamsHelper.CalendarApi;
+using TeamsHelper.TeamsApi;
+
+namespace TeamsHelper.WebApp
+{
+    public interface IGoogleEventValidator
+    {
+        Task<GoogleEventValidationResult> ValidateAsync(GoogleEvent googleEvent, TeamsEvent teamsEvent);
+    }
+}
---
+++ TeamsHelper.WebApp/Types/GoogleEventValidator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TeamsHelper.CalendarApi;
+using TeamsHelper.TeamsApi;
+
+namespace TeamsHelper.WebApp
+{
+    public class GoogleEventValidator : IGoogleEventValidator
+    {
+        public Task<GoogleEventValidationResult> ValidateAsync(GoogleEvent googleEvent, TeamsEvent teamsEvent)
+        {
+            return Task.Run(() =>
+            {
+                GoogleEventValidationResult result = new GoogleEventValidationResult();
+
+                TimeSpan startCompare = (DateTime) googleEvent.Start.DateTime - teamsEvent.Start.DateTime;
+
+                if (startCompare.TotalSeconds > 60 || startCompare.TotalSeconds < 60)
+                {
+                    result.StartingAtValidated = false;
+                }
+
+                TimeSpan endCompare = ((DateTime) googleEvent.End.DateTime) - teamsEvent.End.DateTime;
+            
+                if (endCompare.TotalSeconds > 60 || endCompare.TotalSeconds < 60)
+                {
+                    result.EndingAtValidated = false;
+                }
+
+                return result;
+            });
+        }
+    }
+}
