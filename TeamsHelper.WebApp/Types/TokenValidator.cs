e4866a529e4b410f5e845924e323f61b1124cf51
Kacper Faber <kacperf1234@gmail.com>
2020-10-23 11:42:10 +0200

    Created TokenValidator and AUthorizationVAlidator. Views and VModels.

---
+++ TeamsHelper.WebApp/Types/TokenValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace TeamsHelper.WebApp
+{
+    public class TokenValidator : ITokenValidator
+    {
+        public Task<bool> ValidateAsync(Token token)
+        {
+            return Task.Run(() =>
+                !string.IsNullOrEmpty(token.AccessToken) && string.IsNullOrEmpty(token.RefreshToken) && string.Equals(token.TokenType, "Bearer"));
+        }
+    }
+}
