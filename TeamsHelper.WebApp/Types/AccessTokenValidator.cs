9c7f4e5eadb44ee5a6f0589be84dd66070fda7a4
Kacper Faber <kacperf1234@gmail.com>
2020-10-24 11:41:58 +0200

    Changed OAuthConfiguration, added model IdentityModelKeys.

--- TeamsHelper.WebApp/Types/AccessTokenValidator.cs
+++ TeamsHelper.WebApp/Types/AccessTokenValidator.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace TeamsHelper.WebApp
 {
     public class AccessTokenValidator : IAccessTokenValidator
     {
-        public Task<TokenValidation> ValidateAsync(Token token, OAuthConfiguration authConfiguration)
+        readonly HttpClient _http = new HttpClient();
+        public IIdentityRequestGenerator IdentityRequestGenerator;
+        public ITokenValidationGenerator TokenValidationGenerator;
+
+        public AccessTokenValidator(IIdentityRequestGenerator identityRequestGenerator, ITokenValidationGenerator tokenValidationGenerator)
         {
-            throw new NotImplementedException();
+            IdentityRequestGenerator = identityRequestGenerator;
+            TokenValidationGenerator = tokenValidationGenerator;
+        }
+
+        public async Task<TokenValidation> ValidateAsync(Token token, OAuthConfiguration authConfiguration)
+        {
+            HttpRequestMessage request = IdentityRequestGenerator.Generate(token.AccessToken, authConfiguration);
+            HttpResponseMessage response = await _http.SendAsync(request);
+
+            return await TokenValidationGenerator.GenerateAsync(authConfiguration, response);
         }
     }
 }
---
+++ TeamsHelper.WebApp/Types/IdentityRequestGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+
+namespace TeamsHelper.WebApp
+{
+    public class IdentityRequestGenerator : IIdentityRequestGenerator
+    {
+        public IIdentityRequestMethodProvider RequestMethodProvider;
+
+        public IdentityRequestGenerator(IIdentityRequestMethodProvider requestMethodProvider)
+        {
+            RequestMethodProvider = requestMethodProvider;
+        }
+
+        public HttpRequestMessage Generate(string accessToken, OAuthConfiguration authConfiguration)
+        {
+            HttpRequestMessage req = new HttpRequestMessage
+            {
+                Method = RequestMethodProvider.Provide(authConfiguration),
+                RequestUri = new Uri(authConfiguration.IdentityUrl)
+            };
+            
+            req.Headers.Add("Authorization", "Bearer " + accessToken);
+
+            return req;
+        }
+    }
+}
