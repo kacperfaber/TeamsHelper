c9be3f400856f5ef2914d9ae253554d0828676ec
Kacper Faber <kacperf1234@gmail.com>
2020-10-24 13:41:24 +0200

    Added settings controller.

---
+++ TeamsHelper.WebApp/Controllers/SettingsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TeamsHelper.Database;
+
+namespace TeamsHelper.WebApp
+{
+    [Authorize]
+    public class SettingsController : Controller
+    {
+        public IUserProvider UserProvider;
+
+        public SettingsController(IUserProvider userProvider)
+        {
+            UserProvider = userProvider;
+        }
+
+        public async Task<IActionResult> Settings()
+        {
+            User user = await UserProvider.ProvideAsync(HttpContext);
+            return View(new ViewModel {User = user});
+        }
+    }
+}
