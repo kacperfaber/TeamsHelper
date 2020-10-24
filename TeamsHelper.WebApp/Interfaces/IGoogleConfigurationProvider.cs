cd9fa6c5bf4464b0d0506c7fe7591ac321279b90
Kacper Faber <kacperf1234@gmail.com>
2020-10-24 14:14:11 +0200

    Added GoogleConfigProvider.o

---
+++ TeamsHelper.WebApp/Interfaces/IGoogleConfigurationProvider.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace TeamsHelper.WebApp
+{
+    public interface IGoogleConfigurationProvider
+    {
+        GoogleConfiguration Provide(IConfiguration configuration);
+    }
+}
