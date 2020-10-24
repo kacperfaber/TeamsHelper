4ac5531864b045932647438223b361ccfcce2940
Kacper Faber <kacperf1234@gmail.com>
2020-10-24 12:58:16 +0200

    Added &top parameter into GetEventsUrlGenerator. [Teams]. App will take maximum 1000 events of next 14 days.

--- TeamsHelper.TeamsApi/Types/GetEventsUrlGenerator.cs
+++ TeamsHelper.TeamsApi/Types/GetEventsUrlGenerator.cs
@@ -7,7 +7,7 @@ namespace TeamsHelper.TeamsApi
         public string Generate(TeamsCalendar calendar, DateTime startingAt, DateTime endingAt)
         {
             return
-                $"https://graph.microsoft.com/v1.0/me/calendars/{calendar.Id}/calendarView?startDateTime={DateTimeToString(startingAt)}&endDateTime={DateTimeToString(endingAt)}";
+                $"https://graph.microsoft.com/v1.0/me/calendars/{calendar.Id}/calendarView?startDateTime={DateTimeToString(startingAt)}&endDateTime={DateTimeToString(endingAt)}&top=1000";
         }
 
         string DateTimeToString(DateTime dt) => dt.ToString("yyyy-MM-ddTHH:mm:ssZ");
