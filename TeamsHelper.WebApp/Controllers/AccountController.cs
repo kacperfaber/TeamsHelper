ecc954adb3c1d25e2e4dd5ef75c83c8e4683800d
Kacper Faber <kacperf1234@gmail.com>
2020-10-24 13:17:52 +0200

    Created Delete account functionality.

---
+++ TeamsHelper.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamsHelper.WebApp
+{
+    public class AccountController : Controller
+    {
+        
+    }
+}
---
+++ TeamsHelper.WebApp/Interfaces/IUserDeletor.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using TeamsHelper.Database;
+
+namespace TeamsHelper.WebApp
+{
+    public interface IUserDeletor
+    {
+        Task<bool> DeleteAsync(User user);
+    }
+}
