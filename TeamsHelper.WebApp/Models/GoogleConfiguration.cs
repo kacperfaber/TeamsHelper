961433bb407628736b12aba2fc52e30065ce78dc
Kacper Faber <kacperf1234@gmail.com>
2020-10-24 14:09:57 +0200

    Created GoogleConfiguration with two embedded models.

---
+++ TeamsHelper.WebApp/Models/GoogleConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace TeamsHelper.WebApp
+{
+    public class GoogleConfiguration
+    {
+        public bool DeleteCanceled { get; set; }
+
+        public GoogleConfigurationColors Colors { get; set; }
+
+        public GoogleConfigurationReminders Reminders { get; set; }
+
+        public GoogleConfigurationReminders CanceledReminders { get; set; }
+    }
+}
---
+++ TeamsHelper.WebApp/Models/GoogleConfigurationColors.cs
@@ -0,0 +1,9 @@
+﻿namespace TeamsHelper.WebApp
+{
+    public class GoogleConfigurationColors
+    {
+        public string Active { get; set; }
+
+        public string Canceled { get; set; }
+    }
+}
