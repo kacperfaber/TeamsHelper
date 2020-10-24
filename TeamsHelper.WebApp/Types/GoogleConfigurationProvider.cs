1cb18e331594e8a4b50e51cf263cc411e42006f5
Kacper Faber <kacperf1234@gmail.com>
2020-10-24 14:14:11 +0200

    Added GoogleConfigProvider.o

---
+++ TeamsHelper.WebApp/Types/GoogleConfigurationProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace TeamsHelper.WebApp
+{
+    public class GoogleConfigurationProvider : IGoogleConfigurationProvider
+    {
+        public GoogleConfiguration Provide(IConfiguration configuration)
+        {
+            return configuration.GetSection("GoogleConfiguration").Get<GoogleConfiguration>();
+        }
+    }
+}
